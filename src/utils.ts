ff4f742a9d32e0410dceada49c99e569b3afcdd7
Kelvin Jayanoris <kelvin@jayanoris.com>
2022-12-04 15:23:50 +0300

    Add lookup table stuff

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
-import { Keypair } from "@solana/web3.js";
+import { AnchorProvider } from "@project-serum/anchor";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  TransactionInstruction,
+  TransactionMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
 import fs from "fs";
 
 export function loadKeypair(keypairPath: string): Keypair {
@@ -7,4 +15,57 @@ export function loadKeypair(keypairPath: string): Keypair {
   );
 
   return loaded;
+}
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function sendTransactionV0WithoutLookupTable(
+  provider: AnchorProvider,
+  payer: Keypair,
+  instructions: TransactionInstruction[],
+): Promise<string> {
+  let blockhash = await provider.connection
+    .getLatestBlockhash()
+    .then((res) => res.blockhash);
+
+  const messageV0 = new TransactionMessage({
+    payerKey: payer.publicKey,
+    recentBlockhash: blockhash,
+    instructions,
+  }).compileToV0Message();
+
+  const tx = new VersionedTransaction(messageV0);
+  tx.sign([payer]);
+  return await provider.connection.sendTransaction(tx);
+}
+
+export async function sendTransactionV0WithLookupTable(
+  provider: AnchorProvider,
+  payer: Keypair,
+  lookupTablePubkey: PublicKey,
+  instructions: TransactionInstruction[],
+): Promise<string> {
+  const lookupTableAccount = await provider.connection
+    .getAddressLookupTable(lookupTablePubkey)
+    .then((res) => res.value);
+
+  if (!lookupTableAccount) {
+    throw new Error("lookup table does not exist");
+  }
+
+  let blockhash = await provider.connection
+    .getLatestBlockhash()
+    .then((res) => res.blockhash);
+
+  const messageV0 = new TransactionMessage({
+    payerKey: payer.publicKey,
+    recentBlockhash: blockhash,
+    instructions,
+  }).compileToV0Message([lookupTableAccount]);
+
+  const tx = new VersionedTransaction(messageV0);
+  tx.sign([payer]);
+  return await provider.connection.sendTransaction(tx);
 }
